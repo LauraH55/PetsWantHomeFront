713207849018b697c848357740fbec9c5c7af629
Kevin Besnier <elensar17@gmail.com>
2021-03-19 21:14:08 +0100

    Creation footer composant

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,6 +1,19 @@
 // == Import npm
 import React from 'react';
 
-const Footer = () => <footer></footer>;
+import './footer.scss';
+import Logo from '../../assets/img/B&W-Logo-PWH.png';
+
+const Footer = () => (
+  <footer className="footer">
+    <div className="footer-container">
+      <img className="footer-container-image" src={Logo} alt="logo" />
+      <a href="#">Mentions Légales</a>
+      <a href="#">Contact</a>
+      <a href="#">À propos</a>
+    </div>
+    <p className="footer-copyright">© 2021 PetsWantHome, Tous droits réservés.</p>  
+  </footer>
+);
 
 export default Footer;
