4bfb9fdc8e8f698bc1e9ed160120ce2ad96eb27a
Kevin Besnier <elensar17@gmail.com>
2021-03-19 13:40:14 +0100

    Page home first integration

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,27 @@
 // == Import npm
 import React from 'react';
 
-const Home = () => <main></main>;
+import './home.scss';
+import akita from '../../assets/images/akita.jpg';
+import suivant from '../../assets/images/suivant.png';
+
+const Home = () => (
+  <main className="home">
+    <div className="banner">
+      <h1 className="title">Pets Want Home</h1>
+    </div>
+    <h2 className="subtitle">Quelques animaux disponibles à l'adoption</h2>
+    <img className="thumbnails" src={akita} alt="" />
+    <img className="thumbnails" src={akita} alt="" />
+    <img className="thumbnails" src={akita} alt="" />
+    <img className="thumbnails" src={akita} alt="" />
+    <img className="thumbnails" src={akita} alt="" />
+    <img className="thumbnails" src={akita} alt="" />
+    <img className="thumbnails" src={akita} alt="" />
+    <img className="thumbnails" src={akita} alt="" />
+    <img className="thumbnails" src={suivant} alt="" />
+
+  </main>
+);
 
 export default Home;
